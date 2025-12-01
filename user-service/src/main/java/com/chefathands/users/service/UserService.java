7c5cdb3d37a74211a3610b6d36c9a8c3876f9e63
PAZINAPAST <jan.napast153@gmail.com>
2025-12-01 13:10:03 +0100

    added user-service

---
+++ user-service/src/main/java/com/chefathands/users/service/UserService.java
@@ -0,0 +1,32 @@
+package com.chefatands.users.service;
+
+import com.chefathands.users.model.User;
+import com.chefathands.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public int register(String username, String passwordHash, String email) {
+        if(userRepository.findByUsername(username).isPresent()) {
+            return -1;
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPasswordHash(passwordHash);
+        user.setEmail(email);
+        User saved = userRepository.save(user);
+        return saved.getId();
+    }
+
+    public int exists(String username, String passwordHash) {
+        return userRepository.findByUsernameAndPasswordHash(username, passwordHash)
+                .map(User::getId)
+                .orElse(-1);
+    }
+}
