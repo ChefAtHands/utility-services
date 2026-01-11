2b6e2d144a06ddc7ea5fa5c70dad517bfc0e2237
AnejT <125371242+AnejT@users.noreply.github.com>
2026-01-11 11:36:54 +0100

    testi

---
+++ favourites-service/src/test/java/com/chefathands/favourites/FavouritesServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chefathands.favourites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouritesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
